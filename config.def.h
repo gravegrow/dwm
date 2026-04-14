d9eba7e132f6dc6ed7c7d8a53818d86e7fd1c373
gravegrow <gravegrow@gmail.com>
2026-04-14 18:11:30 +0300

    patch: swallow

--- config.def.h
+++ config.def.h
@@ -4,6 +4,8 @@
 static unsigned int borderpx        = 2;        /* border pixel of windows */
 static unsigned int gappx           = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
+static const int swterminheritfs    = 1;        /* 1 terminal inherits fullscreen on unswallow, 0 otherwise */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 32;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
@@ -49,9 +51,12 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor  unmanaged */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1,      0 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1,      0 },
+	/* class          instance    title            tags mask  isfloating  isterminal  noswallow  monitor unmanaged*/
+	{ "Gimp",         NULL,       NULL,            0,         1,          0,           0,        -1,         0 },
+	{ "Firefox",      NULL,       NULL,            1 << 8,    0,          0,          -1,        -1,         0 },
+	{ "st-256color",  NULL,       NULL,            0,         0,          1,           0,        -1,         0 },
+	{ NULL,           NULL,       "Event Tester",  0,         0,          0,           1,        -1,         0 }, /* xev */
+	{ "Eww",          NULL,       NULL,            0,         0,          0,           0,        -1,         1 },
 };
 
 /* layout(s) */
